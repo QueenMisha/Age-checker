8b66e7f3ce9567a0f27549b12c5e3ee19333ac1d
QueenMisha <misharunwal@gmail.com>
2026-07-17 18:22:19 -0400

    Add files via upload

---
+++ checkage.py
@@ -0,0 +1,13 @@
+# Checking Age 10 - 20 years old
+
+print (" If your 10 or 11 you are probably in Elementry")
+
+if 10/11 == 4/5: True
+
+print (" If you are in Middles school you will probably be 11 - 14 years old")
+
+if 11/14 == 67/8: True
+
+print ("if you are in Highschool you will be probably 14 - 18 years old ")
+
+print (" If older than 18 Then freshman Or collage")
